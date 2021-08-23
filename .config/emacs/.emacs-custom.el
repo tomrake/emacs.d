4ea5a7f4434c1908322e513c147b8c2f1d85da04
Tom Rake <zzzap1957@gmail.com>
2021-08-23 19:15:01 -0400

    Added friendly shell useage.

--- .config/emacs/.emacs-custom.el
+++ .config/emacs/.emacs-custom.el
@@ -13,7 +13,7 @@
    '("b89a4f5916c29a235d0600ad5a0849b1c50fab16c2c518e1d98f0412367e7f97" "b574b0277f72c8e11ff2c92ef208070dbcfa89162424a912f564fc2272be23c7" "e269026ce4bbd5b236e1c2e27b0ca1b37f3d8a97f8a5a66c4da0c647826a6664" "e9460a84d876da407d9e6accf9ceba453e2f86f8b86076f37c08ad155de8223c" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" "d6922c974e8a78378eacb01414183ce32bc8dbf2de78aabcc6ad8172547cb074" "38e64ea9b3a5e512ae9547063ee491c20bd717fe59d9c12219a0b1050b439cdd" "561ba4316ba42fe75bc07a907647caa55fc883749ee4f8f280a29516525fc9e8" default))
  '(fci-rule-color "#383838")
  '(package-selected-packages
-   '(tramp magit dockerfile-mode docker-tramp anaconda-mode popup cyberpunk-theme ac-slime paredit rainbow-delimiters slime epc auto-complete flx-ido))
+   '(shx friendly-remote-shell friendly-shell tramp magit dockerfile-mode docker-tramp anaconda-mode popup cyberpunk-theme ac-slime paredit rainbow-delimiters slime epc auto-complete flx-ido))
  '(safe-local-variable-values '((org-use-property-inheritance . t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
