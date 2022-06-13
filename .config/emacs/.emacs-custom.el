18412f1ef270fce326e58ab2b306dd72325610a3
Tom Rake <zzzap1957@gmail.com>
2022-06-13 08:16:01 -0400

    Cleanup, Modu theme installed.

--- .config/emacs/.emacs-custom.el
+++ .config/emacs/.emacs-custom.el
@@ -8,9 +8,9 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(custom-enabled-themes '(manoj-dark))
+ '(custom-enabled-themes '(modus-operandi))
  '(custom-safe-themes
-   '("b89a4f5916c29a235d0600ad5a0849b1c50fab16c2c518e1d98f0412367e7f97" "b574b0277f72c8e11ff2c92ef208070dbcfa89162424a912f564fc2272be23c7" "e269026ce4bbd5b236e1c2e27b0ca1b37f3d8a97f8a5a66c4da0c647826a6664" "e9460a84d876da407d9e6accf9ceba453e2f86f8b86076f37c08ad155de8223c" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" "d6922c974e8a78378eacb01414183ce32bc8dbf2de78aabcc6ad8172547cb074" "38e64ea9b3a5e512ae9547063ee491c20bd717fe59d9c12219a0b1050b439cdd" "561ba4316ba42fe75bc07a907647caa55fc883749ee4f8f280a29516525fc9e8" default))
+   '("8a332a2fbbd95db0e0ad05c6cb9a42800d4dab92af960cafbe3c553357e1f3c6" "96a4406cca483476d3ce524052bcdd69889a2109c5c3e60be0c8951534d1e0e1"))
  '(fci-rule-color "#383838")
  '(lpr-add-switches t)
  '(lpr-command "lpr.exe")
@@ -23,12 +23,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(rainbow-delimiters-depth-1-face ((t (:inherit rainbow-delimiters-base-face :foreground "dark orange"))))
- '(rainbow-delimiters-depth-2-face ((t (:inherit rainbow-delimiters-base-face :foreground "deep pink"))))
- '(rainbow-delimiters-depth-3-face ((t (:inherit rainbow-delimiters-base-face :foreground "chartreuse"))))
- '(rainbow-delimiters-depth-4-face ((t (:inherit rainbow-delimiters-base-face :foreground "deep sky blue"))))
- '(rainbow-delimiters-depth-5-face ((t (:inherit rainbow-delimiters-base-face :foreground "yellow"))))
- '(rainbow-delimiters-depth-6-face ((t (:inherit rainbow-delimiters-base-face :foreground "orchid"))))
- '(rainbow-delimiters-depth-7-face ((t (:inherit rainbow-delimiters-base-face :foreground "spring green"))))
- '(rainbow-delimiters-depth-8-face ((t (:inherit rainbow-delimiters-base-face :foreground "sienna1"))))
- '(rainbow-delimiters-depth-9-face ((t (:inherit rainbow-delimiters-base-face :foreground "red4")))))
+ '(rainbow-delimiters-depth-1-face ((t nil)))
+ '(rainbow-delimiters-depth-2-face ((t nil)))
+ '(rainbow-delimiters-depth-3-face ((t nil)))
+ '(rainbow-delimiters-depth-4-face ((t nil)))
+ '(rainbow-delimiters-depth-5-face ((t nil)))
+ '(rainbow-delimiters-depth-6-face ((t nil)))
+ '(rainbow-delimiters-depth-7-face ((t nil)))
+ '(rainbow-delimiters-depth-8-face ((t nil)))
+ '(rainbow-delimiters-depth-9-face ((t nil))))
