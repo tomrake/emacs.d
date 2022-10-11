db3d7e3a469a0c0a30a7ddd29e630a12b5059d82
Tom Rake <zzzap1957@gmail.com>
2022-10-11 17:07:33 -0400

    Add use-package-verbose t

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -27,8 +27,8 @@
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 (unless package-archive-contents
@@ -40,6 +40,7 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
+(setq use-package-verbose t)
 
 ;(setq debug-on-error t)
 
