d448cf90a2be88035930cb076708442e271e8602
Tom Rake <zzzap1957@gmail.com>
2022-12-24 11:06:55 -0500

    Fix vertic loading error.

--- .config/emacs/.emacs-custom.el
+++ .config/emacs/.emacs-custom.el
@@ -10,15 +10,15 @@
    ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
  '(custom-enabled-themes '(modus-operandi))
  '(custom-safe-themes
-   '("92d350334df87fe61a682518ff214c773625c6d5ace8060d128adc550bc60c9b" "a0415d8fc6aeec455376f0cbcc1bee5f8c408295d1c2b9a1336db6947b89dd98" "dad40020beea412623b04507a4c185079bff4dcea20a93d8f8451acb6afc8358" "8a332a2fbbd95db0e0ad05c6cb9a42800d4dab92af960cafbe3c553357e1f3c6" "96a4406cca483476d3ce524052bcdd69889a2109c5c3e60be0c8951534d1e0e1"))
+   '("7dc296b80df1b29bfc4062d1a66ee91efb462d6a7a934955e94e786394d80b71" "92d350334df87fe61a682518ff214c773625c6d5ace8060d128adc550bc60c9b" "a0415d8fc6aeec455376f0cbcc1bee5f8c408295d1c2b9a1336db6947b89dd98" "dad40020beea412623b04507a4c185079bff4dcea20a93d8f8451acb6afc8358" "8a332a2fbbd95db0e0ad05c6cb9a42800d4dab92af960cafbe3c553357e1f3c6" "96a4406cca483476d3ce524052bcdd69889a2109c5c3e60be0c8951534d1e0e1"))
  '(fci-rule-color "#383838")
  '(lpr-add-switches t)
  '(lpr-command "lpr.exe")
  '(lpr-headers-switches nil t)
  '(package-selected-packages
-   '(languagetool langtool ## gnu-elpa-keyring-update openwith dired-single pdf-tools diredc visual-fill-column org-present modus-themes org slime-company htmlize shx friendly-remote-shell friendly-shell tramp magit dockerfile-mode docker-tramp anaconda-mode popup cyberpunk-theme ac-slime paredit rainbow-delimiters slime epc auto-complete flx-ido))
+   '(emojify mastodon vertico languagetool langtool ## gnu-elpa-keyring-update openwith dired-single pdf-tools diredc visual-fill-column org-present modus-themes org slime-company htmlize shx friendly-remote-shell friendly-shell tramp magit dockerfile-mode docker-tramp anaconda-mode popup cyberpunk-theme ac-slime paredit rainbow-delimiters slime epc auto-complete flx-ido))
  '(safe-local-variable-values '((org-use-property-inheritance . t)))
- '(warning-suppress-types '((use-package))))
+ '(warning-suppress-types '((initialization) (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -42,7 +42,7 @@
 (setq use-package-verbose t)
 (setq use-package-always-defer t)
 
-;(setq debug-on-error t)
+;   (setq debug-on-error t)
 
 ;;;; Macro to load user customizations from .emacs.d
 (defmacro local-custom-file (file description)
@@ -86,56 +86,56 @@
   (ido-mode t))
 
 ;; Enable vertico
-(use-package vertico
-  :ensure t
-  :init
-  (vertico-mode)
+  (use-package vertico
+;    :ensure t
+    :config
+    (vertico-mode)
+    :custom
+    ;; Different scroll margin
+    ;; (setq vertico-scroll-margin 0)
 
-  ;; Different scroll margin
-  ;; (setq vertico-scroll-margin 0)
+    ;; Show more candidates
+    ;; (setq vertico-count 20)
 
-  ;; Show more candidates
-  ;; (setq vertico-count 20)
+    ;; Grow and shrink the Vertico minibuffer
+     (setq vertico-resize t)
 
-  ;; Grow and shrink the Vertico minibuffer
-   (setq vertico-resize t)
+    ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+    ;; (setq vertico-cycle t)
+    )
 
-  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
-  ;; (setq vertico-cycle t)
-  )
+  ;; Persist history over Emacs restarts. Vertico sorts by history position.
+  (use-package savehist
+    :ensure t
+    :init
+    (savehist-mode))
 
-;; Persist history over Emacs restarts. Vertico sorts by history position.
-(use-package savehist
-  :ensure t
-  :init
-  (savehist-mode))
+  ;; A few more useful configurations...
+  (use-package emacs
+    :init
+    ;; Add prompt indicator to `completing-read-multiple'.
+    ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+    (defun crm-indicator (args)
+      (cons (format "[CRM%s] %s"
+		    (replace-regexp-in-string
+		     "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+		     crm-separator)
+		    (car args))
+	    (cdr args)))
+    (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
 
-;; A few more useful configurations...
-(use-package emacs
-  :init
-  ;; Add prompt indicator to `completing-read-multiple'.
-  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
-  (defun crm-indicator (args)
-    (cons (format "[CRM%s] %s"
-		  (replace-regexp-in-string
-		   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
-		   crm-separator)
-		  (car args))
-	  (cdr args)))
-  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+    ;; Do not allow the cursor in the minibuffer prompt
+    (setq minibuffer-prompt-properties
+	  '(read-only t cursor-intangible t face minibuffer-prompt))
+    (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
 
-  ;; Do not allow the cursor in the minibuffer prompt
-  (setq minibuffer-prompt-properties
-	'(read-only t cursor-intangible t face minibuffer-prompt))
-  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+    ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+    ;; Vertico commands are hidden in normal buffers.
+    ;; (setq read-extended-command-predicate
+    ;;       #'command-completion-default-include-p)
 
-  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
-  ;; Vertico commands are hidden in normal buffers.
-  ;; (setq read-extended-command-predicate
-  ;;       #'command-completion-default-include-p)
-
-  ;; Enable recursive minibuffers
-  (setq enable-recursive-minibuffers t))
+    ;; Enable recursive minibuffers
+    (setq enable-recursive-minibuffers t))
 
 (use-package magit
   :defer 2
@@ -673,6 +673,12 @@
 	(after dired-after-updating-hook first () activate)
 	"Sort dired listings with directories first before adding marks."
 	(mydired-sort))
+
+;;;; mastodon
+  (use-package mastodon
+    :ensure t)
+  (setq mastodon-active-user "tomrake")
+  (setq mastodon-instance-url "https://mastodon.social")
 
 ;; Autommatically tangle our Emacs.org config file when we save it.
 (defun efs/org-babel-tangle-config ()
