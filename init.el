48ea4eb1e694ef4a1be77e9fe816cb6d7988d3dc
Tom Rake <zzzap1957@gmail.com>
2026-03-20 12:24:43 -0400

    Syncing with msys2.

--- init.el
+++ init.el
@@ -137,23 +137,23 @@
 (add-hook 'emacs-startup-hook #'efs/display-startup-time)
 
 (defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el"
-        (or (bound-and-true-p straight-base-dir)
-            user-emacs-directory)))
-      (bootstrap-version 7))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-  (straight-use-package 'use-package)
-  (straight-use-package 'org)
-  (straight-use-package 'emms)
+  (let ((bootstrap-file
+         (expand-file-name
+          "straight/repos/straight.el/bootstrap.el"
+          (or (bound-and-true-p straight-base-dir)
+              user-emacs-directory)))
+        (bootstrap-version 7))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+           'silent 'inhibit-cookies)
+        (goto-char (point-max))
+        (eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
+    (straight-use-package 'use-package)
+;    (straight-use-package 'org)
+    (straight-use-package 'emms)
 
 ;;;; use-package
 (require 'use-package)
