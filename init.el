4dc047be37afd8b304dd09d749b6eb3a0cbf8bc6
Tom Rake <zzzap1957@gmail.com>
2026-03-21 16:23:51 -0400

    Updated straight - Getting started script.

--- init.el
+++ init.el
@@ -132,17 +132,22 @@
 
 (defvar bootstrap-version)
 (let ((bootstrap-file
-	 (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-	(bootstrap-version 5))
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-	  (url-retrieve-synchronously
-	   "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-	   'silent 'inhibit-cookies)
-	(goto-char (point-max))
-	(eval-print-last-sexp)))
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
-  (straight-use-package 'use-package)
+
+    (straight-use-package 'use-package)
+    (straight-use-package '(org :type built-in))
 
 ;;;; use-package
 (require 'use-package)
